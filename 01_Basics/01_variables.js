edfa8394818cc25c438b589c95e4b12c5ddee096
Dibya Ranjan Samal <122545058+Dibya0217@users.noreply.github.com>
2024-03-12 19:40:47 +0000

    #

--- 01_Basics/01_variables.js
+++ 01_Basics/01_variables.js
@@ -1,3 +1,61 @@
+/*
+// Var
+console.log(a); // undefined
+
+var a = 10;
+console.log(a); // 10
+
+a = 20;
+console.log(a); // 20
+
+// Re-declare
+var a = 30;
+console.log(a); // 30
+
+// Initialize without declare
+b = 100;
+console.log(b); // 100
+
+console.log(c); // ReferenceError: c is not defined
+*/
+
+
+/*
+// Let
+// console.log(d); // ReferenceError: Cannot access 'd' before initialization
+let d = 50;
+console.log(d); // 50
+
+// Re-declare
+d = 60;
+console.log(d); // 60
+
+// Declare without initialization
+let e;
+console.log(e); // undefined
+
+// Re-declare
+// let d = 70; // SyntaxError: Identifier 'd' has already been declared
+*/
+
+
+/*
+// const
+// console.log(f); // ReferenceError: Cannot access 'f' before initialization
+const f = 80;
+console.log(f); // 80
+// Updated
+// f = 90;
+// console.log(f); // TypeError: Assignment to constant variable.
+
+// re-declare
+// const f = 90; // SyntaxError: Redeclaration of block-scoped variable 'g'.
+*/
+
+
+
+
+/*
 const accountId = 123
 let accountEmail = "Dibya@gmail.com"
 var accountPassword = "12345"
@@ -16,3 +74,4 @@ accountState = "Karnataka"
 // Prefer not to use var because of issue in block scope and functional scope
 
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+*/
