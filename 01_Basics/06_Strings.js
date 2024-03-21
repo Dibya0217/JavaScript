752555015c13c893c7e7ee101a8a4df20411b7dc
Dibya Ranjan Samal <122545058+Dibya0217@users.noreply.github.com>
2024-03-21 23:59:51 +0530

    #

--- 01_Basics/06_Strings.js
+++ 01_Basics/06_Strings.js
@@ -19,3 +19,180 @@ console.log(str1.charAt(index)); // a
 
 console.log(str1[0]);
 console.log(str1.length);
+
+const str3 = "Dibya";
+const newString = str3.substring(0, 4);
+console.log(newString);
+
+const newString2 = str3.slice(-1, 4);
+console.log(newString2);
+
+const newString3 = "   Dibya   ";
+console.log(newString3);
+console.log(newString3.trim());
+
+const url = "dibyaranjan2002@gmail.com";
+console.log(url.includes( '@' )); // true
+
+/*
+// These methods are present in String
+anchor
+: 
+ƒ anchor()
+at
+: 
+ƒ at()
+big
+: 
+ƒ big()
+blink
+: 
+ƒ blink()
+bold
+: 
+ƒ bold()
+charAt
+: 
+ƒ charAt()
+charCodeAt
+: 
+ƒ charCodeAt()
+codePointAt
+: 
+ƒ codePointAt()
+concat
+: 
+ƒ concat()
+constructor
+: 
+ƒ String()
+endsWith
+: 
+ƒ endsWith()
+fixed
+: 
+ƒ fixed()
+fontcolor
+: 
+ƒ fontcolor()
+fontsize
+: 
+ƒ fontsize()
+includes
+: 
+ƒ includes()
+indexOf
+: 
+ƒ indexOf()
+isWellFormed
+: 
+ƒ isWellFormed()
+italics
+: 
+ƒ italics()
+lastIndexOf
+: 
+ƒ lastIndexOf()
+length
+: 
+0
+link
+: 
+ƒ link()
+localeCompare
+: 
+ƒ localeCompare()
+match
+: 
+ƒ match()
+matchAll
+: 
+ƒ matchAll()
+normalize
+: 
+ƒ normalize()
+padEnd
+: 
+ƒ padEnd()
+padStart
+: 
+ƒ padStart()
+repeat
+: 
+ƒ repeat()
+replace
+: 
+ƒ replace()
+replaceAll
+: 
+ƒ replaceAll()
+search
+: 
+ƒ search()
+slice
+: 
+ƒ slice()
+small
+: 
+ƒ small()
+split
+: 
+ƒ split()
+startsWith
+: 
+ƒ startsWith()
+strike
+: 
+ƒ strike()
+sub
+: 
+ƒ sub()
+substr
+: 
+ƒ substr()
+substring
+: 
+ƒ substring()
+sup
+: 
+ƒ sup()
+toLocaleLowerCase
+: 
+ƒ toLocaleLowerCase()
+toLocaleUpperCase
+: 
+ƒ toLocaleUpperCase()
+toLowerCase
+: 
+ƒ toLowerCase()
+toString
+: 
+ƒ toString()
+toUpperCase
+: 
+ƒ toUpperCase()
+toWellFormed
+: 
+ƒ toWellFormed()
+trim
+: 
+ƒ trim()
+trimEnd
+: 
+ƒ trimEnd()
+trimLeft
+: 
+ƒ trimStart()
+trimRight
+: 
+ƒ trimEnd()
+trimStart
+: 
+ƒ trimStart()
+valueOf
+: 
+ƒ valueOf()
+Symbol(Symbol.iterator)
+: 
+ƒ [Symbol.iterator]()
+*/
---
+++ 01_Basics/07_Numbers_and_Maths.js
@@ -0,0 +1,56 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance); // 100
+
+console.log(balance.toString());
+console.log(typeof balance.toString());
+console.log(balance.toFixed(1));
+
+const otherNumber = 1123.8966
+console.log(otherNumber.toPrecision(6));
+
+const hundreds = 1000000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(Number.MIN_SAFE_INTEGER);
+
+
+console.log(Math.abs(-4));
+console.log(Math.abs(4));
+console.log(Math.round(4.6));
+console.log(Math.round(4.2));
+console.log(Math.floor(4.6));
+console.log(Math.ceil(4.6));
+console.log(Math.min(4, 3, 6, 8));
+console.log(Math.max(4, 3, 6, 8));
+
+
+/*
+// These methods are present in String
+constructor
+: 
+ƒ Number()
+toExponential
+: 
+ƒ toExponential()
+toFixed
+: 
+ƒ toFixed()
+toLocaleString
+: 
+ƒ toLocaleString()
+toPrecision
+: 
+ƒ toPrecision()
+toString
+: 
+ƒ toString()
+valueOf
+: 
+ƒ valueOf()
+*/
